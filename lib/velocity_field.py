821f9146ee001a358181160b49c6e131843bfd32
annadeg <annagdeg@gmail.com>
2021-12-10 15:08:44 +0100

    added velocity dispersion

--- lib/velocity_field.py
+++ lib/velocity_field.py
@@ -4,9 +4,10 @@ from astropy.io import fits
 
 class VelField2D(object):
     
-    def __init__(self, VelCurve1D):
+    def __init__(self, VelCurve1D,VelDisp1D):
         self.__velcurve1D = VelCurve1D
-        
+        self.__veldisp1D = VelDisp1D
+
     def model(self, x, y, parameters_dict):
         
         #Taking necessary parameters from parameter dictonary with 
@@ -24,17 +25,20 @@ class VelField2D(object):
         # Mapping 1D velocity curve onto the grid
         if parameters_dict["inclination"] == 90:
             velocity = self.__velcurve1D(x_rot, parameters_dict)
-            pscale = np.diff(y)[0][0]
+            veldisp = self.__veldisp1D(x_rot, parameters_dict)                        
+            pscale = np.diff(y_rot)[0][0]
             select = np.abs(y_rot) > (pscale/2)
             velocity[select] = 0.
+            veldisp[select] = 0.            
 
         else:
             r = np.sqrt(x_rot**2+(y_rot**2/cos2i))        
             velocity = self.__velcurve1D(r, parameters_dict)*(x_rot/r)*sini
             select = r==0
             velocity[select] = 0.0 
+            veldisp = self.__veldisp1D(r, parameters_dict)
         
-        return velocity
+        return velocity, veldisp
 
 
 class VelCurve1D(object):
@@ -45,21 +49,36 @@ class VelCurve1D(object):
         velocity = self.__function1D(r, parameter_dict)
         return velocity
     
+class VelDisp1D(object):
+    def __init__(self, function):
+        self.__function1D = function
+        
+    def model(self, r, parameter_dict):
+        veldisp = self.__function1D(r, parameter_dict)
+        return veldisp
+
 
 def ArcTan1D(r, parameters_dict):
     v_out = (2.0 / np.pi) * parameters_dict['v_asympt'] * np.arctan(r / parameters_dict['r_turnover'])
     return v_out
 
 
+def ConstantVdisp(r,parameters_dict):
+    return np.ones(r.shape)*parameters_dict['sigma_0']
+
+
+
+
+
 if __name__ == "__main__":
     r = np.arange(-10,10,0.05)
-    parameters = {"v_asympt":200, "r_turnover":0.2, "PA": 20.0, "inclination": 80.0, 'cent_x_vel':0.0, 'cent_y_vel':0.0,}
-    vel_2D = VelField2D(ArcTan1D)
+    parameters = {"v_asympt":200, "r_turnover":0.2, "PA": 20.0, "inclination": 90.0, 'cent_x_vel':0.0, 'cent_y_vel':0.0,'sigma_0':50.}
+    vel_2D = VelField2D(ArcTan1D,ConstantVdisp)
     
     
     size_x = 3.0
     size_y = 3.0
-    sampling = 0.1
+    sampling = 0.05
     xs= np.linspace(size_x/-2.0,size_x/2.0,int(size_x/sampling))
     ys= np.linspace(size_y/-2.0,size_y/2.0,int(size_y/sampling))
     xs,ys = np.meshgrid(xs, ys)
@@ -70,5 +89,5 @@ if __name__ == "__main__":
     #pix_scale = 0.01
     #print((y-y_cent)*pix_scale,ys*pix_scale)
     vel_field = vel_2D.model(xs,ys, parameters)
-    hdu = fits.PrimaryHDU(vel_field)
-    hdu.writeto('test_vel.fits',overwrite=True)
+    #hdu = fits.PrimaryHDU(vel_field)
+    #hdu.writeto('test_vel.fits',overwrite=True)
