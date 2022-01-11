1ba9c6d2ad678a3bd3f148cc2fc0f41564fe864a
husemann@mpia.de <husemann@mpia.de>
2022-01-11 10:14:38 +0100

    added IPS file writing function

--- lib/cube.py
+++ lib/cube.py
@@ -42,6 +42,42 @@ class Cube(object):
         hdu.header['CRPIX3'] = 1
         hdu.writeto(fileout, overwrite=True)
         
+    def writeIPSObject(self, fileout, author="anonymous", date="2000-01-10T00:00:00", description="data"):
+        xpos=[]
+        ypos=[]
+        wave_array = []
+        flux_array = []
+        dim = self.__x.shape
+    
+        for ix in range(dim[1]):
+            for iy in range(dim[0]):
+                xpos.append(self.__x[iy,ix])
+                ypos.append(self.__y[iy,ix])
+                wave_array.append(self.__wave*1e-6)
+                flux_array.append(self.data[:,iy,ix])
+        column=[]
+        column.append(fits.Column(name="XPOS",format="E",unit='arcsec', array=xpos))
+        column.append(fits.Column(name="YPOS",format="E",unit='arcsec',array=ypos))
+        column.append(fits.Column(name="NBVAL",format="I",unit='UNITLESS',array=np.ones(len(ypos),dtype=np.int16)*len(self.__wave)))
+        column.append(fits.Column(name="WAVELENGTH",format="%dE"%(len(self.__wave)),unit='m',array=wave_array))
+        column.append(fits.Column(name="FLUX",format="%dE"%(len(self.__wave)),unit='W m-2 m-1 [arcsec-2]',array=flux_array))
+    
+    
+        hdu = fits.PrimaryHDU()
+        hdu.header['DATE'] = date
+        hdu.header['AUTHOR'] = author
+        hdu.header['ID'] = 1
+        hdu.header['DESCR'] = description
+        hdu.header['FILETYPE'] = 'SRC-ES'
+        hdu.header['CONTFLAG'] = 'F'
+        hdu.header['EMIFLAG'] = 'F'
+        hdu.header['NBADD'] = '1'
+        hdu.header['PM'] = 'NOT APPLICABLE'
+        tab = fits.BinTableHDU.from_columns(column)
+        tab.header['EXTNAME']='ADD001'
+        hdus = fits.HDUList([hdu,tab])
+        hdus.writeto(fileout,overwrite=True)
+        
 if __name__ == "__main__":
     parameters = {'r_eff_light':0.2, 'n_index':1.0, 'cent_x_light':0.0, 'cent_y_light':0.0, 'axisratio_light':np.cos(50.0/180.0*np.pi), 'PA_light':20.0, 'total_flux':10.0, "v_asympt":200, "r_turnover":0.2, "PA": 20.0, "inclination": 50.0, 'cent_x_vel':0.0, 'cent_y_vel':0.0,'sigma_0':50.}
     
@@ -57,6 +93,7 @@ if __name__ == "__main__":
     Light2D = LightDistribution2D(Sersic2D)
     Cube3D = Cube(xs, ys, wave)
     Cube3D.computeLine([Light2D], [vel2D], [parameters], 0.65648, 3.0)
-    Cube3D.writeFitsData("test_cube.fits")
+    #Cube3D.writeFitsData("test_cube.fits")
+    Cube3D.writeIPSObject("test_cube.fits")
     
     
